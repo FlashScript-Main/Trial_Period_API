884f97120154161f1aa9a3b37bcda49f16d8ac5f
FlashScript <itzflashscript@gmail.com>
2024-12-22 09:54:42 +0330

    Test #10

--- src/app/api/check/route.ts
+++ src/app/api/check/route.ts
@@ -1,10 +1,11 @@
-import { getUsers } from "@/utils/db";
-// import { NextResponse } from "next/server"
+// import { getUsers } from "@/utils/db";
 
+import { users } from "../test/test";
+
+// import { NextResponse } from "next/server"
 export const GET = async () => {
     // const newUUID = uuidv4();
     // return Response.json(newUUID);
-    const users = getUsers();
     
     return Response.json(
         { users },
